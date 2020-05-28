76ac438aea78c7d7828b4d64944473a9bddef73c
Aavarosan <56538884+Aavarosan@users.noreply.github.com>
2020-05-28 20:16:09 +0200

    Aktualizacja testów #3

--- battleship_test.py
+++ battleship_test.py
@@ -16,13 +16,11 @@ class TileTest(unittest.TestCase):
 
     def test_reset_tile_result_is_revealed(self):
         self.tile.reset_tile()
-        result = self.tile.is_revealed
-        self.assertFalse(result)
+        self.assertFalse(self.tile.is_revealed)
 
     def test_reset_tile_result_is_ship(self):
         self.tile.reset_tile()
-        result = self.tile.is_ship
-        self.assertFalse(result)
+        self.assertFalse(self.tile.is_ship)
 
     def test_show_tile_result(self):
         self.tile.show_tile()
@@ -30,31 +28,26 @@ class TileTest(unittest.TestCase):
 
     def test_tile_info_return_after_reset(self):
         self.tile.reset_tile()
-        test_data = self.tile.tile_info()
-        self.assertEqual(test_data, '000')
+        self.assertEqual(self.tile.tile_info(), '000')
 
     def test_tile_info_return_after_show(self):
         self.tile.show_tile()
-        test_data = self.tile.tile_info()
-        self.assertEqual(test_data, '100')
+        self.assertEqual(self.tile.tile_info(), '100')
 
     def test_update_tile_result_if_revealed(self):
         test_message = b'1111'
         self.tile.update_tile(test_message)
-        result = self.tile.is_revealed
-        self.assertTrue(result)
+        self.assertTrue(self.tile.is_revealed)
 
     def test_update_tile_result_if_ship(self):
         test_message = b'1110'
         self.tile.update_tile(test_message)
-        result = self.tile.is_ship
-        self.assertTrue(result)
+        self.assertTrue(self.tile.is_ship)
 
     def test_update_tile_result_if_shot(self):
         test_message = b'0111'
         self.tile.update_tile(test_message)
-        result = self.tile.is_shot
-        self.assertTrue(result)
+        self.assertTrue(self.tile.is_shot)
 
     def test_update_tile_returns_type_list(self):
         test_message = b'0101'
@@ -96,8 +89,7 @@ class GameGridTest(unittest.TestCase):
     def test_get_tile_returns_false(self):
         x_test = 600
         y_test = 300
-        result = self.grid.get_tile(x_test, y_test)
-        self.assertFalse(result)
+        self.assertFalse(self.grid.get_tile(x_test, y_test))
 
 
 class MessageTest(unittest.TestCase):
@@ -114,8 +106,7 @@ class MessageTest(unittest.TestCase):
         self.assertEqual(result, [1, 1, 2])
 
     def test_enemy_shot_decoder_returns_value(self):
-        result = battleship.enemy_shot_decoder(self.message_2)
-        self.assertEqual(result, [0, 1, 1])
+        self.assertEqual(battleship.enemy_shot_decoder(self.message_2), [0, 1, 1])
 
 
 if __name__ == '__main__':
